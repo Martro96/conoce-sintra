f85847b15bcf6b80ba11effdaa16ed63d326c962
Marta Rodrigo <martarodrigo.55@gmail.com>
2024-03-30 21:54:42 +0100

    añado los archivos al repositorio para poder ver el error del script parcel:build

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,52 @@
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+/*SWIPER*/
+new Swiper(".swiper", {
+	direction: "horizontal",
+	loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev"
+	},
+
+	autoplay: {
+		delay: 5000,
+	},
+    breakpoints: {
+    // when window width is >= 320px
+    250: {
+    slidesPerView: 1,
+    spaceBetween: 50
+    },
+    // when window width is >= 768 pantalla desktop
+    768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+    },
+    // when window width is >=900
+    900: {
+        slidesPerView: 3,
+        spaceBetween: 30
+    }
+    }
+});
+
+
+/****MENÚ HAMBURGUESA******/
+document.addEventListener("DOMContentLoaded", function() {
+    const nav = document.querySelector("#nav");
+    const abrir = document.querySelector("#abrir");
+    const cerrar = document.querySelector("#cerrar");
+
+    abrir.addEventListener("click", () => {
+        nav.classList.add("main-nav--visible"); 
+    });
+
+    cerrar.addEventListener("click", () => {
+        nav.classList.remove("main-nav--visible"); 
+    });
+});
